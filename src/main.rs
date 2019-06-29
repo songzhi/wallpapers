34d6bacf82a3bf9029ee195deb32d32606e113be
lsongzhi <lsongzhi@163.com>
2019-06-29 08:16:56 +0800

    more configurable

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 #![windows_subsystem = "windows"]
 
 use wallpapers::run_as_daemon;
-use toml;
-use wallpapers::Orientation;
+
 fn main() {
-//    let x = toml::from_str::<Orientation>("landscape").unwrap();
-    let y = toml::to_string(&Orientation::Landscape).unwrap();
-    println!("{:?}", y);
+    run_as_daemon();
 }
